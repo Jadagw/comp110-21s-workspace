7b972b9a6c23c52e9bc0b930122263d9e34037f4
Jada Willse <jadagw@email.unc.edu>
2021-01-26 16:55:18 -0500

    lesson 1/26

---
+++ lessons/input.py
@@ -0,0 +1,7 @@
+"""A program to demonstrate user input and variables."""
+
+### below: declared a variable
+name: str = input("Who are you? ")
+
+print("Wow, " + name + ", you rock!")
+print(name + " have a great day!")
